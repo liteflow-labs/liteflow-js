d65eadaabb78dd9fecbfda15eba9013c6a7e5108
antho1404 <anthony.estebe@gmail.com>
2022-12-03 23:05:37 +0700

    remove providers from components

--- packages/components/src/app.tsx
+++ packages/components/src/app.tsx
@@ -1,74 +1,21 @@
-import { ApolloProvider, NormalizedCacheObject } from '@apollo/client'
 import Bugsnag from '@bugsnag/js'
 import BugsnagPluginReact from '@bugsnag/plugin-react'
 import { ChakraProvider } from '@chakra-ui/react'
 import { Dict } from '@chakra-ui/utils'
-import { Web3Provider } from '@ethersproject/providers'
-import { LiteflowProvider } from '@nft/hooks'
-import { Web3ReactProvider } from '@web3-react/core'
-import React, {
-  ComponentType,
-  Fragment,
-  PropsWithChildren,
-  useMemo,
-} from 'react'
-import { CookiesProvider } from 'react-cookie'
-import Cookies from 'universal-cookie'
-import { getAndEnrichClient } from './client'
-import Session, {
-  COOKIE_ADDRESS,
-  COOKIE_JWT_TOKEN,
-  Props as SessionProps,
-} from './session'
+import React, { ComponentType, Fragment, PropsWithChildren } from 'react'
+import Session from './session'
 
-type Props = PropsWithChildren<
-  {
-    endpointUri: string
-    ssr: boolean
-    cache: NormalizedCacheObject
-    user?: {
-      address?: string
-      token?: string
-    }
-    theme: Dict
-    bugsnagAPIKey?: string
-  } & SessionProps
->
-
-function web3Provider(provider: any): Web3Provider {
-  return new Web3Provider(
-    provider,
-    typeof provider.chainId === 'number'
-      ? provider.chainId
-      : typeof provider.chainId === 'string'
-      ? parseInt(provider.chainId)
-      : 'any',
-  )
-}
+type Props = PropsWithChildren<{
+  theme: Dict
+  bugsnagAPIKey?: string
+}>
 
 export default function LiteflowNFTApp({
-  endpointUri,
-  ssr,
-  cache,
-  user,
   theme,
   bugsnagAPIKey,
   children,
   ...sessionProps
 }: Props): JSX.Element {
-  const cookies = useMemo(() => {
-    const res: { [key: string]: string } = {}
-    if (user?.address) {
-      res[COOKIE_ADDRESS] = user.address
-      if (user.token) res[COOKIE_JWT_TOKEN(user.address)] = user.token
-    }
-    return res
-  }, [user])
-  const client = useMemo(
-    () => getAndEnrichClient(endpointUri, ssr, cookies, cache),
-    [endpointUri, ssr, cookies, cache],
-  )
-
   if (bugsnagAPIKey) {
     Bugsnag.start({
       apiKey: bugsnagAPIKey,
@@ -82,15 +29,7 @@ export default function LiteflowNFTApp({
   return (
     <ErrorBoundary>
       <ChakraProvider theme={theme}>
-        <ApolloProvider client={client}>
-          <Web3ReactProvider getLibrary={web3Provider}>
-            <CookiesProvider cookies={ssr ? new Cookies(cookies) : undefined}>
-              <LiteflowProvider endpoint={endpointUri}>
-                <Session {...sessionProps}>{children}</Session>
-              </LiteflowProvider>
-            </CookiesProvider>
-          </Web3ReactProvider>
-        </ApolloProvider>
+        <Session {...sessionProps}>{children}</Session>
       </ChakraProvider>
     </ErrorBoundary>
   )
--- packages/components/src/props.tsx
+++ packages/components/src/props.tsx
@@ -1,7 +1,15 @@
-import { ApolloClient, NormalizedCacheObject } from '@apollo/client'
+import { NextApiRequestCookies } from 'next/dist/server/api-utils'
+import Cookies from 'universal-cookie'
+import { setContext } from '@apollo/client/link/context'
+import {
+  createHttpLink,
+  InMemoryCache,
+  ApolloClient,
+  NormalizedCacheObject,
+} from '@apollo/client'
+import decode, { JwtPayload } from 'jwt-decode'
 import { GetServerSidePropsContext, GetServerSidePropsResult } from 'next'
-import { APOLLO_STATE_PROP_NAME, getClient } from './client'
-import { COOKIE_ADDRESS, COOKIE_JWT_TOKEN } from './session'
+import { COOKIE_JWT_TOKEN } from './session'
 
 type UserProp = {
   address: string | null
@@ -16,6 +24,50 @@ type GetServerSidePropsContextWithUser = GetServerSidePropsContext & {
   user: UserProp
 }
 
+export const APOLLO_STATE_PROP_NAME = '__APOLLO_STATE__'
+
+function getClient(
+  uri: string,
+  ssrMode: boolean,
+  cookies: NextApiRequestCookies | undefined,
+): ApolloClient<NormalizedCacheObject> {
+  const httpLink = createHttpLink({
+    uri,
+  })
+
+  // this function is called every time apollo is making a request
+  const authLink = setContext((_, context) => {
+    const c = new Cookies(cookies)
+
+    const jwtToken = c.get(COOKIE_JWT_TOKEN)
+    if (!jwtToken) return context
+
+    // check expiration of jwt token
+    const res = decode<JwtPayload>(jwtToken)
+    if (res.exp && res.exp < Math.ceil(Date.now() / 1000)) return context
+
+    return {
+      ...context,
+      headers: {
+        ...context.headers,
+        authorization: 'Bearer ' + jwtToken,
+      },
+    }
+  })
+
+  return new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache({
+      typePolicies: {
+        Account: {
+          keyFields: ['address'],
+        },
+      },
+    }),
+    ssrMode,
+  })
+}
+
 export function wrapServerSideProps<T extends { [key: string]: unknown }>(
   url: string,
   handler: (
@@ -27,15 +79,15 @@ export function wrapServerSideProps<T extends { [key: string]: unknown }>(
 ) => Promise<GetServerSidePropsResult<T>> {
   return async (context) => {
     const client = getClient(url, true, context.req.cookies)
+    const jwt = context.req.cookies[COOKIE_JWT_TOKEN]
+    const address = jwt
+      ? decode<JwtPayload & { address: string }>(jwt).address
+      : null
     const contextWithUser = {
       ...context,
       user: {
-        address: context.req.cookies[COOKIE_ADDRESS] || null,
-        token: context.req.cookies[COOKIE_ADDRESS]
-          ? context.req.cookies[
-              COOKIE_JWT_TOKEN(context.req.cookies[COOKIE_ADDRESS])
-            ] || null
-          : null,
+        address: address,
+        token: jwt || null,
       },
     }
     const result = await handler(contextWithUser, client)
