dac3b681f06f921ef49e63ffc7e740c916d750a8
antho1404 <anthony.estebe@gmail.com>
2022-12-20 11:28:19 +0700

    update dependencies

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,11 +1,11 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-  unstable_staticImage: true,
-  unstable_flexsearch: {
+  staticImage: true,
+  flexsearch: {
     codeblocks: true,
   },
-  unstable_defaultShowCopyCode: true,
+  defaultShowCopyCode: true,
 })
 module.exports = withNextra({
   reactStrictMode: true,
