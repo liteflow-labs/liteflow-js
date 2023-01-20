ab1e121fee4b0b530ae38936a8491a0c55c10f29
antho1404 <anthony.estebe@gmail.com>
2023-01-20 14:02:40 +0700

    reorg imports

--- example/connector.ts
+++ example/connector.ts
@@ -1,6 +1,6 @@
 import { configureChains, createClient, goerli, mainnet } from 'wagmi'
-import { InjectedConnector } from 'wagmi/connectors/injected'
 import { bsc, bscTestnet, polygon, polygonMumbai } from 'wagmi/chains'
+import { InjectedConnector } from 'wagmi/connectors/injected'
 import { publicProvider } from 'wagmi/providers/public'
 
 export const { chains, provider } = configureChains(
