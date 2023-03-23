59eb9d1ebbeb1e439f8b9b949b88409e52644db3
antho1404 <anthony.estebe@gmail.com>
2023-03-23 11:33:31 +0700

    remove extraneous cast

--- packages/core/src/utils/convert.ts
+++ packages/core/src/utils/convert.ts
@@ -1,11 +1,11 @@
 import type { Address, ChainId, TransactionHash } from '../types'
 
 export function toAddress(address: string): Address {
-  return `0x${address.toLowerCase().replace(/^0x/, '')}` as Address
+  return `0x${address.toLowerCase().replace(/^0x/, '')}`
 }
 
 export function toTransactionHash(hash: string): TransactionHash {
-  return `0x${hash.toLowerCase().replace(/^0x/, '')}` as TransactionHash
+  return `0x${hash.toLowerCase().replace(/^0x/, '')}`
 }
 
 export function toId(keys: (string | undefined | null)[]): string {
