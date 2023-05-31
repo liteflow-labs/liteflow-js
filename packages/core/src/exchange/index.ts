6598a5f8666c549fe5dc693ea179a667ed5c3f09
antho1404 <anthony.estebe@gmail.com>
2023-05-31 16:30:39 +0700

    add cancelListing and cancelBid functions to core

--- packages/core/src/exchange/index.ts
+++ packages/core/src/exchange/index.ts
@@ -1,5 +1,6 @@
 import type { Signer } from 'ethers'
-import type { Sdk } from '../graphql'
+import invariant from 'ts-invariant'
+import type { FetchOfferQuery, Sdk } from '../graphql'
 import type { Uint256, UUID } from '../types'
 import type { State as AcceptOfferState } from './acceptOffer'
 import { acceptOffer } from './acceptOffer'
@@ -33,12 +34,20 @@ export class Exchange {
     return listToken(this.sdk, listing, signer, onProgress)
   }
 
-  async cancelOffer(
-    offerId: UUID,
+  async cancelBid(
+    bidId: UUID,
     signer: Signer,
     onProgress?: (state: CancelOfferState) => void,
   ): Promise<UUID> {
-    return cancelOffer(this.sdk, offerId, signer, onProgress)
+    return cancelOffer(this.sdk, bidId, signer, onProgress)
+  }
+
+  async cancelListing(
+    listingId: UUID,
+    signer: Signer,
+    onProgress?: (state: CancelOfferState) => void,
+  ): Promise<UUID> {
+    return cancelOffer(this.sdk, listingId, signer, onProgress)
   }
 
   async acceptOffer(
@@ -48,5 +57,15 @@ export class Exchange {
     onProgress?: (state: AcceptOfferState) => void,
   ): Promise<UUID> {
     return acceptOffer(this.sdk, offerId, quantity, signer, onProgress)
+  }
+
+  // Low level API to retrieve an offer
+  // Offer can be either a bid or a listing
+  async getOffer(
+    offerId: UUID,
+  ): Promise<NonNullable<FetchOfferQuery['offer']>> {
+    const { offer } = await this.sdk.FetchOffer({ offerId })
+    invariant(offer, "Offer doesn't exist")
+    return offer
   }
 }
--- packages/hooks/src/useCancelOffer.ts
+++ packages/hooks/src/useCancelOffer.ts
@@ -26,22 +26,34 @@ export default function useCancelOffer(signer: Signer | undefined): [
     CancelOfferStep.INITIAL,
   )
 
+  // Create a callback for the onProgress event
+  const onProgress = useCallback(
+    (state) => {
+      switch (state.type) {
+        case 'TRANSACTION_SIGNATURE':
+          setActiveProcess(CancelOfferStep.TRANSACTION_SIGNATURE)
+          break
+        case 'TRANSACTION_PENDING':
+          setActiveProcess(CancelOfferStep.TRANSACTION_PENDING)
+          setTransactionHash(state.payload.txHash)
+          break
+      }
+    },
+    [setActiveProcess, setTransactionHash],
+  )
+
   const cancelOffer: CancelOfferFn = useCallback(
     async ({ id }) => {
       invariant(signer, ErrorMessages.SIGNER_FALSY)
-      await client.exchange.cancelOffer(id, signer, (state) => {
-        switch (state.type) {
-          case 'TRANSACTION_SIGNATURE':
-            setActiveProcess(CancelOfferStep.TRANSACTION_SIGNATURE)
-            break
-          case 'TRANSACTION_PENDING':
-            setActiveProcess(CancelOfferStep.TRANSACTION_PENDING)
-            setTransactionHash(state.payload.txHash)
-            break
-        }
-      })
+      const offer = await client.exchange.getOffer(id)
+      invariant(offer)
+      if (offer.type === 'SALE') {
+        await client.exchange.cancelListing(id, signer, onProgress)
+      } else {
+        await client.exchange.cancelBid(id, signer, onProgress)
+      }
     },
-    [client.exchange, signer],
+    [client, signer, onProgress],
   )
 
   return [cancelOffer, { activeStep, transactionHash }]
