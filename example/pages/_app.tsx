a1243cd09492b1165ff4c4a291d0b17b444cac7d
Nicolas Mahe <nicolas@mahe.me>
2024-05-17 16:14:20 +0700

    update example app

--- example/pages/_app.tsx
+++ example/pages/_app.tsx
@@ -3,29 +3,29 @@ import { AppProps } from 'next/app'
 import {
   WagmiConfig,
   configureChains,
-  createClient,
-  goerli,
+  createConfig,
   mainnet,
+  sepolia,
 } from 'wagmi'
 import { bsc, bscTestnet, polygon, polygonMumbai } from 'wagmi/chains'
 import { InjectedConnector } from 'wagmi/connectors/injected'
 import { publicProvider } from 'wagmi/providers/public'
 import styles from '../styles/app.module.css'
 
-export const { chains, provider } = configureChains(
-  [mainnet, goerli, bscTestnet, bsc, polygon, polygonMumbai],
+const { publicClient } = configureChains(
+  [mainnet, sepolia, bscTestnet, bsc, polygon, polygonMumbai],
   [publicProvider()],
 )
 
-export const client = createClient({
+const config = createConfig({
+  publicClient,
   autoConnect: true,
   connectors: [new InjectedConnector()],
-  provider,
 })
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <WagmiConfig client={client}>
+    <WagmiConfig config={config}>
       <LiteflowProvider apiKey={process.env.NEXT_PUBLIC_LITEFLOW_API_KEY}>
         <div className={styles.app}>
           <Component {...pageProps} />
